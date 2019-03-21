e73464ae8d5c6a1ed8c79b2808cef40fc2661f0b
wanda4 <wanda4@kauplan.org>
2019-03-21 13:51:09 +0900

    add files

---
+++ lib/terraformdsl.rb
@@ -0,0 +1,6 @@
+require "terraformdsl/version"
+
+module Terraformdsl
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/terraformdsl/version.rb
@@ -0,0 +1,3 @@
+module Terraformdsl
+  VERSION = "0.1.0"
+end
